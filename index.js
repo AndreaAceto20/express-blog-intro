22f789cd1a65a9b0e293863ec19e80822dd4f37a
Andrea Aceto <andrea.aceto20@gmail.com>
2025-02-04 17:37:49 +0100

    [MOD]Percorsi immagini

--- index.js
+++ index.js
@@ -15,31 +15,31 @@ app.get(`/bacheca`, (req, res) => {
         {
             titolo: "ciambellone",
             contenuto: "foto di ciambellone",
-            img: "http://localhost:3000/images/ciambellone.jpeg",
+            img: "./images/ciambellone.jpeg",
             tags: "dolci, fatto in casa, ciambellone",
         },
         {
             titolo: "cracker barbabietola",
             contenuto: "foto di cracker alla barbabietola",
-            img: "http://localhost:3000/images/cracker_barbabietola.jpeg",
+            img: "./images/cracker_barbabietola.jpeg",
             tags: "cracker, fatto in casa",
         },
         {
             titolo: "pane fritto",
             contenuto: "foto di pane fritto dolce",
-            img: "http://localhost:3000/images/pane_fritto_dolce.jpeg",
+            img: "./images/pane_fritto_dolce.jpeg",
             tags: "dolci, pane fritto",
         },
         {
             titolo: "pasta barbabietola",
             contenuto: "foto di pasta alla barbabietola",
-            img: "http://localhost:3000/images/pasta_barbabietola.jpeg",
+            img: "./images/pasta_barbabietola.jpeg",
             tags: "pasta",
         },
         {
             titolo: "torta paesana",
             contenuto: "foto di torta paesana",
-            img: "http://localhost:3000/images/torta_paesana.jpeg",
+            img: "./images/torta_paesana.jpeg",
             tags: "dolci, torta paesana",
         },
     ];
